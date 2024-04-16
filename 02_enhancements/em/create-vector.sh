3174cee1131661cc688610cb2346fdbfab436b44
M4XMU3 <56072399+M4XMU3@users.noreply.github.com>
2024-04-16 20:14:40 +0200

    read api_key from env

--- 02_enhancements/em/create-vector.sh
+++ 02_enhancements/em/create-vector.sh
@@ -3,9 +3,8 @@
 # variables
 model="text-embedding-ada-002"
 url="https://api.openai.com/v1/embeddings"
-api_key="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"
 page=$1
-
+api_key=$(export $(xargs .env) && env | grep OPENAI_API_KEY | awk -F'=' '{print $2}')
 vector=$(curl -X POST -H "Content-Type: application/json" \
      -H "Authorization: Bearer $api_key" \
      -d '{
